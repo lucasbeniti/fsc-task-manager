cc1cb6d9540b7e7ccc24a48f3c94f121324805f0
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-01-26 11:10:06 -0300

    feat: add Tasks and Button components

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+const Button = ({ children, variant = "primary" }) => {
+  const getVariantClasses = () => {
+    if (variant === "primary") {
+      return "bg-[#00ADB5] text-white";
+    }
+    if (variant === "ghost") {
+      return "bg-transparent text-[#818181]";
+    }
+  };
+  return (
+    <button
+      className={`flex items-center gap-1 rounded-md px-3 py-1 text-xs font-semibold ${getVariantClasses()} transition-all hover:opacity-85`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
