784c9af978f57b7dd96a42d849ed57de615c9bde
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-01-26 15:38:21 -0300

    feat: add simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
   { ignores: ["dist"] },
@@ -22,6 +23,7 @@ export default [
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -34,6 +36,8 @@ export default [
         { allowConstantExport: true },
       ],
       "react/prop-types": "off",
+      "simple-import-sort/imports": "warn ",
+      "simple-import-sort/exports": "warn ",
     },
   },
 ];
--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -1,11 +1,10 @@
-// eslint-disable-next-line react/prop-types
 const SidebarButton = ({ children, variant }) => {
   const getVariantClasses = () => {
-    if (variant === 'selected') {
-      return 'bg-[#E6F7F8] text-[#00ADB5] ';
+    if (variant === "selected") {
+      return "bg-[#E6F7F8] text-[#00ADB5] ";
     }
-    if (variant === 'unselected') {
-      return 'text-[#35383E] ';
+    if (variant === "unselected") {
+      return "text-[#35383E] ";
     }
   };
   return (
